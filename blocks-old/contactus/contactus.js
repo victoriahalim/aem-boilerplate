ef121da1bc584b971ef9932e04178358420a8050
victoriahalim <vhalim@deloitte.com.au>
2024-04-24 13:47:38 +1000

    packages

--- blocks-old/contactus/contactus.js
+++
@@ -1,6 +0,0 @@
-export default function decorate(block){
-    const allDivs= block.querySelectorAll('div');
-    allDivs.forEach(function(div){
-        div.classList.add('contactus-details');
-    });
-}
